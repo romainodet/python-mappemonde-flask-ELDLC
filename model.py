97c1c8f622fc114c932ff5b1f0e2ca8b5bfbc00e
antoine.scherrer <antoine.scherrer@lecole-ldlc.com>
2015-11-23 17:19:09 +0100

    add files

---
+++ model.py
@@ -0,0 +1,70 @@
+import sqlite3
+import sys
+
+class Model:
+    #Constructor, connect to database
+    def __init__(self):
+        self.con = sqlite3.connect('example.sqlite');
+        self.cur = self.con.cursor()
+
+    def __enter__(self):
+        return self
+
+    #Desctructor, disconnect from database
+    def __exit__(self, type, value, traceback):
+        if(self.con):
+          self.con.close()
+
+    #Get the content of the Artist table
+    def getArtistsSimple(self):
+        return self.sqlQuery("""
+                            SELECT * FROM [Artist]
+                            """)
+
+    #Get the content of the Tracks table
+    def getTracksSimple(self):
+        return self.sqlQuery("""
+                            SELECT * FROM [Track]
+                             """)
+
+    #Get the content of the Album table
+    def getAlbumsSimple(self):
+        return self.sqlQuery("""
+                            SELECT * FROM [Track]
+                             """)
+
+    #Get the content of the Artsit Table and count number of album for each artist
+    def getArtists(self):
+        return self.sqlQuery("""
+                            SELECT ar.ArtistId,ar.Name, COUNT(al.ArtistId)
+                            FROM [Artist] as ar, [Album] as al
+                            WHERE ar.ArtistId = al.ArtistId
+                            GROUP BY al.ArtistId
+                            ORDER BY ar.Name ASC
+                            """)
+
+    #Display track information
+    def getTracks(self):
+        return self.sqlQuery("""
+                            SELECT t.TrackId as num,t.Name as track_name,al.Title as album_title,ar.Name as artist
+                            FROM
+                            [Track] as t
+                            LEFT JOIN [Album] as al ON t.AlbumId=al.AlbumId
+                            LEFT JOIN [Artist] as ar ON al.ArtistId=ar.ArtistId
+                            ORDER BY al.Title ASC
+                             """)
+
+    #Display album information
+    def getAlbums(self):
+        return self.sqlQuery("""
+                            SELECT al.AlbumId as num,al.Title as album_name,ar.Name as artist
+                            FROM
+                            [Album] as al
+                            LEFT JOIN [Artist] as ar ON al.ArtistId=ar.ArtistId
+                            ORDER BY ar.Name ASC
+                             """)
+
+    #Execute an SQL query and returns the result
+    def sqlQuery(self,q):
+        self.cur.execute(q)
+        return self.cur.fetchall()
---
+++ webserver.py
@@ -0,0 +1,55 @@
+from model import Model
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Show artists
+@app.route('/artists_simple.html')
+def artists_simple():
+    with Model() as model:
+      artists=model.getArtistsSimple()
+      return render_template('artists_simple.html', artists=artists)
+
+@app.route('/artists.html')
+def artists():
+    with Model() as model:
+      artists=model.getArtists()
+      return render_template('artists.html', artists=artists)
+
+#Show tracks
+@app.route('/tracks_simple.html')
+def tracks_simple():
+    with Model() as model:
+      tracks=model.getTracksSimple()
+      return render_template('tracks_simple.html', tracks=tracks)
+
+@app.route('/tracks.html')
+def tracks():
+    with Model() as model:
+      tracks=model.getTracks()
+      return render_template('tracks.html', tracks=tracks)
+
+
+#Show albums
+@app.route('/albums_simple.html')
+def albums_simple():
+    with Model() as model:
+      albums=model.getAlbumsSimple()
+      return render_template('albums_simple.html', albums=albums)
+
+@app.route('/albums.html')
+def albums():
+    with Model() as model:
+      albums=model.getAlbums()
+      return render_template('albums.html', albums=albums)
+
+# new routes should be defined here
+
+
+# main application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
